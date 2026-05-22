1c08213b5cb2328bbf0c8db61062d6c8102da7f5
Rik <ricardomartins145@gmail.com>
2026-05-22 17:55:52 +0100

    Refactor session error handling

--- backend/src/routes/sessionRoutes.ts
+++ backend/src/routes/sessionRoutes.ts
@@ -1,13 +1,14 @@
-import { Router }           from "express";
-import { authenticateToken } from "../middleware/authMiddleware";
+import { Router }             from "express";
+import { authenticateToken }  from "../middleware/authMiddleware";
 import * as sessionController from '../controllers/sessionController';
+import { asyncHandler }       from "../utils/asyncHandler";
 
 const router = Router();
 
-router.get('/', authenticateToken, sessionController.getMySessions);
-router.get('/:id', authenticateToken, sessionController.getMySessionById);
-router.post('/', authenticateToken, sessionController.createSession);
-router.patch('/:id', authenticateToken, sessionController.updateMySession);
-router.delete('/:id', authenticateToken, sessionController.deleteMySession);
+router.get('/', authenticateToken, asyncHandler(sessionController.getMySessions));
+router.get('/:id', authenticateToken, asyncHandler(sessionController.getMySessionById));
+router.post('/', authenticateToken, asyncHandler(sessionController.createSession));
+router.patch('/:id', authenticateToken, asyncHandler(sessionController.updateMySession));
+router.delete('/:id', authenticateToken, asyncHandler(sessionController.deleteMySession));
 
 export default router;
